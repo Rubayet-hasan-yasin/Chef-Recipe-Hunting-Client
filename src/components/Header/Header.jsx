8d5125f0aa7d3d104ebbc927a181d5527a18493e
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-02 12:07:49 +0600

    responsive header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,145 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { HiMenu, HiX } from "react-icons/hi";
+import { GiCook } from "react-icons/gi";
+
+
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     const user = true;
     const name = true
-    const [mouseEnter, setMouseEnter] = useState(false)
-    console.log(mouseEnter);
     return (
-        <div className='h-24 w-full px-16 py-10 flex items-center' >
-            <h1 className='flex-grow text-4xl font-extrabold text-gray-600'>Chef's Plate</h1>
+        <div className=' px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+            <div className='relative flex items-center justify-between'>
+                {/* Logo Section */}
+                <Link to='/' className='inline-flex items-center'>
+                    <GiCook className='h-10 w-10 text-gray-600' />
+                    <span className='ml-1 text-4xl font-bold tracking-wide text-gray-600'>
+                        Chef's Plate
+                    </span>
+                </Link>
 
-            <ul className='flex space-x-12 text-base font-medium'>
+                {/* Nav Items Section */}
+                <nav className='lg:flex items-center hidden'>
+                    <ul className='items-center space-x-8 flex'>
+                        <li>
+                            <NavLink
+                                to='/'
+                                className={({ isActive }) => (isActive ? 'active' : 'default')}
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to='/Blog'
+                                className={({ isActive }) => (isActive ? 'active' : 'default')}
+                            >
+                                Blog
+                            </NavLink>
+                        </li>
+                    </ul>
 
-                <li>
-                    <NavLink to={'/'}>Home</NavLink>
-                </li>
+                    {
+                        user ?
+                            <div className='overflow-hidden relative transition duration-200 transform ml-12'>
+                                <img
+                                    src='https://i.ibb.co/dbb1Vkh/f48e2701-903c-42b2-a70d-443cdea9f4d3-removebg-preview.png'
+                                    alt='profile'
+                                    className='object-cover w-20'
+                                />
 
-                <li>
-                    <NavLink to={'blog'}>Blog</NavLink>
-                </li>
-            </ul>
+                                {
+                                    name &&
+                                    <div className='bg-white opacity-0 hover:opacity-100 text-gray-900 absolute inset-0 transition-opacity duration-200 flex flex-col rounded-xl'>
+                                        <p className='text-xl font-extrabold text-center m-auto'>yasin</p>
+                                    </div>
+                                }
+                            </div>
+                            :
+                            <button className='ml-12 py-3 px-7 bg-[#F9A51A] rounded-lg shadow-md'>Login</button>
+                    }
+                </nav>
+                {/* Mobile Navbar Section */}
+                <div className='lg:hidden'>
+                    {/* Dropdown Open Button */}
+                    <button
+                        aria-label='Open Menu'
+                        title='Open Menu'
+                        onClick={() => setIsMenuOpen(true)}
+                    >
+                        <HiMenu />
+                    </button>
+                    {isMenuOpen && (
+                        <div className='absolute top-0 left-0 w-full z-10'>
+                            <div className='p-5 bg-white border rounded shadow-sm'>
+                                {/* Logo & Button section */}
+                                <div className='flex items-center justify-between mb-4'>
+                                    <div>
+                                        <Link to='/' className='inline-flex items-center'>
+                                        <GiCook className='h-10 w-10 text-gray-600' />
+                                            <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                                                Chef's Plate
+                                            </span>
+                                        </Link>
+                                    </div>
+                                    {/* Dropdown menu close button */}
+                                    <div>
+                                        <button
+                                            aria-label='Close Menu'
+                                            title='Close Menu'
+                                            onClick={() => setIsMenuOpen(false)}
+                                        >
+                                            <HiX className='w-5 text-gray-600' />
+                                        </button>
+                                    </div>
+                                </div>
+                                {/* Mobile Nav Items Section */}
+                                <nav>
+                                    <ul className='space-y-4'>
+                                        <li>
+                                            <Link to='/' className='default'>
+                                                Home
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to='/blog'
+                                                className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                            >
+                                                Blog
+                                            </Link>
+                                        </li>
+                                    </ul>
 
-            {
-                user ?
-                    <div className='overflow-hidden relative transition duration-200 transform ml-12'>
-                        <img
-                            src='https://i.ibb.co/dbb1Vkh/f48e2701-903c-42b2-a70d-443cdea9f4d3-removebg-preview.png'
-                            alt='profile'
-                            className='object-cover w-20'
-                        />
+                                    {
+                                        user ?
+                                            <div className='overflow-hidden relative transition duration-200 transform ml-12'>
+                                                <img
+                                                    src='https://i.ibb.co/dbb1Vkh/f48e2701-903c-42b2-a70d-443cdea9f4d3-removebg-preview.png'
+                                                    alt='profile'
+                                                    className='object-cover w-20'
+                                                />
 
-                        {
-                            name && 
-                            <div className='bg-white opacity-0 hover:opacity-100 text-gray-900 absolute inset-0 transition-opacity duration-200 flex flex-col rounded-xl'>
-                            <p className='text-xl font-extrabold text-center m-auto'>yasin</p>
+                                                {
+                                                    name &&
+                                                    <div className='bg-white opacity-0 hover:opacity-100 text-gray-900 absolute inset-0 transition-opacity duration-200 flex flex-col rounded-xl'>
+                                                        <p className='text-xl font-extrabold text-center m-auto'>yasin</p>
+                                                    </div>
+                                                }
+                                            </div>
+                                            :
+                                            <button className='ml-12 py-3 px-7 bg-[#F9A51A] rounded-lg shadow-md'>Login</button>
+                                    }
+                                </nav>
+                            </div>
                         </div>
-                        }
-                    </div>
-                    :
-                    <button className='ml-12 py-3 px-7 bg-[#F9A51A] rounded-lg shadow-md'>Login</button>
-            }
+                    )}
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default Header;
