fa4b2de8ca8f237e6fb3804a932102d08102ce30
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-03 12:41:54 +0600

    recipes card added

--- src/components/Card/ChefRecipesCard.jsx
+++ src/components/Card/ChefRecipesCard.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ChefRecipesCard = ({ recipe }) => {
+    const [readmore, setReadMore] = useState(false);
     const { image_url, recipe_name, ingredients, cooking_method, rating } = recipe;
-    // console.log(recipe);
+    
     return (
         <div className='border px-5 py-6 rounded-lg shadow-xl'>
             <div className='grid md:grid-cols-2 gap-4'>
                 <div>
-                    <h1 className='text-4xl my-7 text-gray-700 border-b pb-1'>INGREDIENTS</h1>
+
+                    <h1 className='text-5xl my-7 text-orange-600 border-b pb-3 font-bold'>{recipe_name}</h1>
+                    <h1 className='text-3xl my-7 text-gray-700 border-b pb-1'>INGREDIENTS</h1>
                     <ol className='list-disc ml-8 text-gray-500'>
                         {
                             ingredients.map((ing, i) => <li key={i}>{ing}</li>)
@@ -15,13 +18,23 @@ const ChefRecipesCard = ({ recipe }) => {
                     </ol>
                 </div>
 
-                <img className='w-52 h-fit rounded-lg shadow-xl' src={image_url} alt="img" />
+                <img className='w-fit h-fit rounded-lg shadow-xl' src={image_url} alt="img" />
 
 
             </div>
             <div>
-                <h4 className='text-4xl my-8 text-gray-700 border-b pb-2'>Cooking Method</h4>
-                <p className='text-gray-600'>{cooking_method}</p>
+                <h4 className='text-3xl my-8 text-gray-700 border-b pb-2'>Cooking Method</h4>
+                <p className='text-gray-600'>
+                    {
+                        readmore ? cooking_method : cooking_method.slice(0, 300)+' ' 
+                    }
+                    <button
+                        onClick={() => setReadMore(!readmore)}
+                        className='text-blue-600 font-semibold underline'
+                    >
+                        {readmore ? " read less" : " read more ...."}
+                    </button>
+                </p>
             </div>
         </div>
     );
